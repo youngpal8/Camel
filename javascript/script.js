73a863b49426b57177177acc132168a1874830ce
YOUNG-PAL <kat01brown@gmail.com>
2024-06-11 12:06:11 +0100

    my commits

---
+++ javascript/script.js
@@ -0,0 +1,292 @@
+document.getElementById('wee').style.backgroundColor= 'red'
+document.getElementById('wee').style.paddingBottom= '15px'
+document.getElementById('wee').style.paddingTop= '18px'
+
+//document.getElementById('hds').style.textAlign= 'center'
+//document.getElementById('hds').style.fontFamily= 'Times New Roman'
+//document.getElementById('hds').style.fontStyle= 'Normal'
+//document.getElementById('hds').style.color= 'blue'
+//document.getElementById('hds').style.marginTop= '35px'
+
+document.getElementById('xox1').style.marginLeft= '540px'
+document.getElementById('xox1').style.marginTop= '35px'
+
+//document.getElementById('saw').style.textAlign= 'center'//
+document.getElementById('saw').style.marginTop= '-18px'
+document.getElementById('saw').style.marginLeft= '570px'
+
+document.getElementById('xox2').style.marginLeft= '742px'
+document.getElementById('xox2').style.marginTop= '-10px'
+
+//document.getElementsByClassName('bless').style.display= 'flex'
+
+document.getElementById('flo').style.position= 'absolute'
+document.getElementById('flo').style.marginLeft= '680px'
+
+document.getElementById('wlo').style.position= 'absolute'
+document.getElementById('wlo').style.marginLeft= '538px'
+document.getElementById('wlo').style.marginTop= '-10px'
+
+
+//document.getElementById('clo').style.textAlign= 'center'//
+document.getElementById('clo').style.marginLeft= '570px'
+
+document.getElementById('trud').style.position= 'absolute'
+document.getElementById('trud').style.marginLeft= '150px'
+document.getElementById('trud').style.size= '120%'
+
+document.getElementById('wolf').style.textDecoration= 'none'
+document.getElementById('wolf').style.backgroundColor= 'red'
+document.getElementById('wolf').style.fontSize= '13px'
+document.getElementById('wolf').style.marginLeft= '768px'
+document.getElementById('wolf').style.padding= '2px'
+document.getElementById('wolf').style.color= 'white'
+
+// document.getElementById('lion').style.marginLeft= '545px'
+// document.getElementById('lion').style.fontSize= '25px'
+// document.getElementById('lion').style.fontWeight= 'bold'
+// document.getElementById('lion').style.color= 'gray'
+// document.getElementById('lion').style.position= 'absolute'
+// document.getElementById('lion').style.textDecoration= 'none'
+// document.getElementById('lion').style.textAlign= 'center'
+
+document.getElementById('wage').style.marginLeft= '775px'
+document.getElementById('wage').style.position= 'absolute'
+document.getElementById('wage').style.fontSize= '12px'
+document.getElementById('wage').style.marginTop= '120px'
+
+document.getElementById('gov').style.position= 'absolute'
+document.getElementById('gov').style.fontSize= '16px'
+document.getElementById('gov').style.marginTop= '160px'
+document.getElementById('gov').style.marginLeft= '538px'
+document.getElementById('gov').style.textAlign= 'center'
+
+document.getElementById('car').style.marginTop= '250px'
+
+document.getElementById('ann').style.marginLeft= '150px'
+document.getElementById('ann').style.position= 'absolute'
+
+document.getElementById('doso').style.marginLeft= '768px'
+document.getElementById('doso').style.position= 'absolute'
+document.getElementById('doso').style.textDecoration= 'none'
+document.getElementById('doso').style.marginTop= '12px'
+document.getElementById('doso').style.backgroundColor= 'red'
+document.getElementById('doso').style.color= 'white'
+document.getElementById('doso').style.padding= '2px'
+document.getElementById('doso').style.fontSize= '13px'
+
+//document.getElementById('pal').style.position= 'absolute'
+//document.getElementById('pal').style.marginLeft= '538px'
+//document.getElementById('pal').style.marginTop= '50px'
+//document.getElementById('pal').style.fontSize= '25px'
+//document.getElementById('pal').style.textAlign= 'center'
+//document.getElementById('kol').style.textDecoration= 'none'
+//document.getElementById('kol').style.fontWeight= 'bold'
+
+//document.getElementById('int').style.color= 'gray'
+
+document.getElementById('fanta').style.position= 'absolute'
+document.getElementById('fanta').style.marginLeft= '768px'
+document.getElementById('fanta').style.marginTop= '156px'
+document.getElementById('fanta').style.fontSize= '12px'
+
+document.getElementById('drink').style.marginLeft= '538px'
+document.getElementById('drink').style.marginTop= '200px'
+document.getElementById('drink').style.position= 'absolute'
+document.getElementById('drink').style.textAlign= 'center'
+
+document.getElementById('recycle').style.position= 'absolute'
+document.getElementById('recycle').style.marginLeft= '146px'
+document.getElementById('recycle').style.marginTop= '265px'
+
+document.getElementById('bottle').style.marginTop= '300px'
+document.getElementById('bottle').style.marginLeft= '150px'
+document.getElementById('bottle').style.position= 'absolute'
+
+document.getElementById('orange').style.position= 'absolute'
+document.getElementById('orange').style.backgroundColor= 'red'
+document.getElementById('orange').style.marginTop= '300px'
+document.getElementById('orange').style.marginLeft= '768px'
+document.getElementById('orange').style.fontSize= '13px'
+document.getElementById('orange').style.padding= '2px'
+
+document.getElementById('sodium').style.position= 'absolute'
+document.getElementById('sodium').style.marginLeft= '768px'
+document.getElementById('sodium').style.marginTop= '441px'
+document.getElementById('sodium').style.fontSize= '12px'
+
+document.getElementById('fibre').style.position= 'absolute'
+document.getElementById('fibre').style.marginTop= '485px'
+document.getElementById('fibre').style.marginLeft= '538px'
+document.getElementById('fibre').style.textAlign= 'center'
+
+document.getElementById('sweet').style.position= 'absolute'
+document.getElementById('sweet').style.marginLeft= '146px'
+document.getElementById('sweet').style.marginTop= '570px'
+
+document.getElementById('cerpac').style.position= 'absolute'
+document.getElementById('cerpac').style.marginTop= '600px'
+document.getElementById('cerpac').style.marginLeft= '150px'
+
+document.getElementById('layers').style.position= 'absolute'
+document.getElementById('layers').style.marginLeft= '768px'
+document.getElementById('layers').style.marginTop= '750px'
+document.getElementById('layers').style.fontSize= '12px'
+
+document.getElementById('vegan').style.position= 'absolute'
+document.getElementById('vegan').style.marginTop= '800px'
+document.getElementById('vegan').style.marginLeft= '538px'
+document.getElementById('vegan').style.textAlign= 'center'
+
+document.getElementById('mango').style.position= 'absolute'
+document.getElementById('mango').style.marginLeft= '146px'
+document.getElementById('mango').style.marginTop= '870px'
+
+document.getElementById('keep').style.position= 'absolute'
+document.getElementById('keep').style.marginTop= '900px'
+document.getElementById('keep').style.marginLeft= '150px'
+
+document.getElementById('derm').style.position= 'absolute'
+document.getElementById('derm').style.marginLeft= '768px'
+document.getElementById('derm').style.marginTop= '1050px'
+document.getElementById('derm').style.fontSize= '12px'
+
+document.getElementById('rich').style.position= 'absolute'
+document.getElementById('rich').style.marginTop= '1100px'
+document.getElementById('rich').style.marginLeft= '538px'
+document.getElementById('rich').style.textAlign= 'center'
+
+document.getElementById('upper').style.position= 'absolute'
+document.getElementById('upper').style.marginLeft= '146px'
+document.getElementById('upper').style.marginTop= '1170px'
+
+document.getElementById('young').style.marginLeft= '568px'
+document.getElementById('young').style.position= 'absolute'
+document.getElementById('young').style.marginTop= '27px'
+
+document.getElementById('cruel').style.position= 'absolute'
+document.getElementById('cruel').style.marginLeft= '600px'
+document.getElementById('cruel').style.marginTop= '20px'
+
+document.getElementById('old').style.marginTop= '27px'
+document.getElementById('old').style.position= 'absolute'
+document.getElementById('old').style.marginLeft= '794px'
+
+document.getElementById('soyabean').style.textDecoration= 'none'
+document.getElementById('soyabean').style.backgroundColor= 'red'
+document.getElementById('soyabean').style.fontSize= '12px'
+document.getElementById('soyabean').style.position= 'absolute'
+document.getElementById('soyabean').style.marginTop= '-45px'
+document.getElementById('soyabean').style.marginLeft= '12px'
+document.getElementById('soyabean').style.color= 'white'
+document.getElementById('soyabean').style.padding= '2px'
+
+document.getElementById('native').style.textDecoration= 'none'
+document.getElementById('native').style.backgroundColor= 'red'
+document.getElementById('native').style.fontSize= '12px'
+document.getElementById('native').style.padding= '2px'
+document.getElementById('native').style.color= 'white'
+document.getElementById('native').style.position= 'absolute'
+document.getElementById('native').style.marginTop= '-45px'
+document.getElementById('native').style.marginLeft= '12px'
+
+document.getElementById('bearer').style.textDecoration= 'none'
+document.getElementById('bearer').style.backgroundColor= 'red'
+document.getElementById('bearer').style.fontSize= '12px'
+document.getElementById('bearer').style.padding= '2px'
+document.getElementById('bearer').style.color= 'white'
+document.getElementById('bearer').style.position= 'absolute'
+document.getElementById('bearer').style.marginTop= '-45px'
+document.getElementById('bearer').style.marginLeft= '12px'
+
+document.getElementById('osun').style.textDecoration= 'none'
+document.getElementById('osun').style.backgroundColor= 'red'
+document.getElementById('osun').style.fontSize= '12px'
+document.getElementById('osun').style.padding= '2px'
+document.getElementById('osun').style.color= 'white'
+document.getElementById('osun').style.position= 'absolute'
+document.getElementById('osun').style.marginTop= '-45px'
+document.getElementById('osun').style.marginLeft= '12px'
+
+document.getElementById('court').style.textDecoration= 'none'
+document.getElementById('court').style.backgroundColor= 'red'
+document.getElementById('court').style.fontSize= '12px'
+document.getElementById('court').style.padding= '2px'
+document.getElementById('court').style.color= 'white'
+document.getElementById('court').style.position= 'absolute'
+document.getElementById('court').style.marginTop= '-45px'
+document.getElementById('court').style.marginLeft= '12px'
+
+document.getElementById('diary').style.textDecoration= 'none'
+document.getElementById('diary').style.backgroundColor= 'red'
+document.getElementById('diary').style.fontSize= '12px'
+document.getElementById('diary').style.padding= '2px'
+document.getElementById('diary').style.color= 'white'
+document.getElementById('diary').style.position= 'absolute'
+document.getElementById('diary').style.marginTop= '-45px'
+document.getElementById('diary').style.marginLeft= '12px'
+
+document.getElementById('same').style.textDecoration= 'none'
+document.getElementById('same').style.backgroundColor= 'red'
+document.getElementById('same').style.fontSize= '12px'
+document.getElementById('same').style.padding= '2px'
+document.getElementById('same').style.color= 'white'
+document.getElementById('same').style.position= 'absolute'
+document.getElementById('same').style.marginTop= '-45px'
+document.getElementById('same').style.marginLeft= '12px'
+
+document.getElementById('male').style.textDecoration= 'none'
+document.getElementById('male').style.backgroundColor= 'red'
+document.getElementById('male').style.fontSize= '12px'
+document.getElementById('male').style.padding= '2px'
+document.getElementById('male').style.color= 'white'
+document.getElementById('male').style.position= 'absolute'
+document.getElementById('male').style.marginTop= '-45px'
+document.getElementById('male').style.marginLeft= '12px'
+
+// document.getElementById('permit').style.marginLeft= '568px'
+// document.getElementById('permit').style.position= 'absolute'
+// document.getElementById('permit').style.marginTop= '5px'
+
+// document.getElementById('only').style.position= 'absolute'
+// document.getElementById('only').style.marginLeft= '600px'
+// document.getElementById('only').style.marginTop= '3px'
+
+// document.getElementById('old').style.marginTop= '5px'
+// document.getElementById('old').style.position= 'absolute'
+// document.getElementById('old').style.marginLeft= '794px'
+
+
+document.getElementById('assy').style.marginLeft= '165px'
+document.getElementById('assy').style.position= 'absolute'
+document.getElementById('assy').style.marginTop= '70px'
+
+document.getElementById('grip').style.position= 'absolute'
+document.getElementById('grip').style.marginLeft= '200px'
+document.getElementById('grip').style.marginTop= '63px'
+document.getElementById('grip').style.color= 'white'
+
+document.getElementById('clip').style.marginTop= '70px'
+document.getElementById('clip').style.position= 'absolute'
+document.getElementById('clip').style.marginLeft= '328px'
+
+document.getElementById('aiming').style.marginLeft= '961px'
+document.getElementById('aiming').style.position= 'absolute'
+document.getElementById('aiming').style.marginTop= '70px'
+
+document.getElementById('shooting').style.position= 'absolute'
+document.getElementById('shooting').style.marginLeft= '993px'
+document.getElementById('shooting').style.marginTop= '63px'
+document.getElementById('shooting').style.color= 'white'
+
+document.getElementById('brush').style.marginTop= '70px'
+document.getElementById('brush').style.position= 'absolute'
+document.getElementById('brush').style.marginLeft= '1210px'
+
+//document.getElementById('apple').style.marginTop= '50px'
+
+
+
+
+
+
